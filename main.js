330dbd53054d00a5315b051e5244d187ee6820c9
Daiyrbek Artelov <dartelov@intelliants.com>
2021-11-14 04:12:59 +0600

    refactor: SRP

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import InvalidArgumentError from './src/Errors/InvalidArgumentError.js';
 import CommandLineArgumentsParser from './src/Support/CommandLineArgumentsParser.js';
 import ConfigValidator from './src/Support/ConfigValidator.js';
 import configExtractor from './src/Support/ConfigExtractor.js';
+import pipeline from 'stream';
 
 const configValidator = new ConfigValidator(configExtractor);
 
@@ -10,7 +11,16 @@ const cliParser = new CommandLineArgumentsParser(process.argv);
 const config = cliParser.getConfig()
 
 try {
-    configValidator.validate(config)
+    (async () => {
+        const streams = await configValidator.validate(config)
+        console.log(streams)
+        pipeline(
+            process.stdin,
+            ...streams,
+            process.stdout,
+            err => console.log(err)
+        );
+    })
 } catch (err) {
     if (err instanceof InvalidArgumentError) {
         console.log(err.message);
--- src/Support/CommandLineArgumentsParser.js
+++ src/Support/CommandLineArgumentsParser.js
@@ -1,17 +1,40 @@
 export default class CommandLineArgumentsParser {
-    constructor(argv) {
-        this._argv = argv;
+  constructor(argv) {
+    this._argv = argv;
+  }
+
+  get() {
+    return {
+      config: this.getConfig(),
+      input: this.getInput(),
+      output: this.getOutput(),
+    };
+  }
+
+  getConfig() {
+    const index = this._getIndex('--config', '-c')
+
+    return this._argv[index];
+  }
+
+  _getIndex(flag, shortFlag) {
+    let index = this._argv.findIndex((arg) => arg === flag);
+    if (index !== -1) {
+        return index + 1;
     }
 
-    getConfig() {
-        let index = this._argv.findIndex(arg => arg === '--config');
+    index = this._argv.findIndex((arg) => arg === shortFlag);
 
-        if (index === -1) {
-            index = this._argv.findIndex(arg => arg === '-c');
-        }
-
-        const configIndex = index + 1;
-
-        return this._argv[configIndex];
+    if (index !== -1) {
+        return index + 1;
     }
-};
+
+    return null;
+  }
+
+  getInput() {
+
+  }
+
+  getOutput() {}
+}
