17e938fbb2bef8eef3b033526457a4934847a743
Daiyrbek Artelov <dartelov@intelliants.com>
2021-11-21 03:14:18 +0600

    add stream factory test for readable and writable streams

--- tests/factories/streams-factory.test.js
+++ tests/factories/streams-factory.test.js
@@ -2,8 +2,10 @@ import streamsFactory from '../../src/factories/streams-factory.js'
 import Caesar from "../../src/streams/ciphers/caesar";
 import Atbash from "../../src/streams/ciphers/atbash";
 import ROT from "../../src/streams/ciphers/rot";
+import FileReadable from "../../src/streams/file-readable";
+import FileWritable from "../../src/streams/file-writable";
 
-describe('streams factory test', () => {
+describe('streams factory: create ciphers test', () => {
     const ciphers = [
         ["caesar", Caesar],
         ["rot", ROT],
@@ -26,3 +28,28 @@ describe('streams factory test', () => {
         expect(result[0]).toBeInstanceOf(Atbash)
     })
 });
+
+describe('streams factory: create readable test', () => {
+    test('it creates stdin stream if input file is not given', async () => {
+        const result = await streamsFactory.createReadable(null)
+        expect(result).toBe(process.stdin)
+    })
+
+    test('it creates readable stream if input file is given', async () => {
+        const result = await streamsFactory.createReadable('input.txt')
+        expect(result).toBeInstanceOf(FileReadable)
+    })
+})
+
+
+describe('streams factory: create writable test', () => {
+    test('it creates stdout stream if output file is not given', async () => {
+        const result = await streamsFactory.createWritable(null)
+        expect(result).toBe(process.stdout)
+    })
+
+    test('it creates writable stream if output file is given', async () => {
+        const result = await streamsFactory.createWritable('output.txt')
+        expect(result).toBeInstanceOf(FileWritable)
+    })
+})
